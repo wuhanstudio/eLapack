8141f864a6c52de35bfd18ee5d98ab564619882f
Wu Han <wuhanstudio@qq.com>
2019-08-05 12:03:56 +0800

    Fix warnings and Add examples

---
+++ Examples/example.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <LinearAlgebra/declareFunctions.h>
+
+ int main(int argc, char const *argv[])
+{
+    // Start
+    clock_t start, end;
+    float cpu_time_used;
+    start = clock();
+
+     // Math
+    double A[3*3] = {3,  4, 1,
+                     6, 10, 2,
+                     5,  3, 2};
+
+    double X[3*4];
+
+    double B[3*4] = {4, 3, 22, 5,
+                     5, 3,  2, 5,
+                     3, 4,  5, 3};
+
+    linsolve(A, X, B, 3, 4); // 3 = A row, 4 = B column
+
+    print(X, 3, 4);
+
+     // End
+    end = clock();
+    cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nTotal speed  was %f ms\n", cpu_time_used * 1000);
+
+     return 0;
+}
